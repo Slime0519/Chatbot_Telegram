15081951967e91b2042b603f51f01f3991c1e2c5
leeju <slime0519@gist.ac.kr>
2020-07-10 00:30:09 +0900

    implementation basic messaging code edit .gitignore(add my html request token)

---
+++ Receive_Message.py
@@ -0,0 +1,24 @@
+from HTML_API_TOKEN import API_TOKEN
+import requests
+import json
+
+#토큰 받아오기
+MyToken = API_TOKEN()
+token = MyToken.GetToken()
+
+#봇의 메시지 내용 가져오기 : https://core.telegram.org/bots/api
+url = 'https://api.telegram.org/bot{}/getUpdates'.format(token)
+#url = 'https://api.telegram.org/bot{}/getMe'.format(token)
+response = json.loads(requests.get(url).text)
+
+#sendMessage method : 메시지 보내기
+url = 'https://api.telegram.org/bot{}/sendMessage'.format(token)
+
+#response parsing해서 id, text따오기
+chat_id = response['result'][-1]['message']['from']['id']
+chat_text = response['result'][-2]['message']['text']
+
+#send message
+requests.get(url, params={'chat_id':chat_id, 'text':"i'm junmyeong bot"})
+
+
